07ee56291a706615950bfa638849cc9de6598f76
latif <latifirfansyah@gmail.com>
2024-08-26 14:08:08 +0700

    add dependency injection

--- src/test/java/com/acc/springcore/service/impl/EmployeeServiceImpl1.java
+++ src/test/java/com/acc/springcore/service/impl/EmployeeServiceImpl1.java
@@ -1,8 +1,10 @@
 package com.acc.springcore.service.impl;
 
 import com.acc.springcore.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
+@Qualifier("emp1")
 public class EmployeeServiceImpl1 implements EmployeeService {
 }
--- src/test/java/com/acc/springcore/service/impl/EmployeeServiceImpl2.java
+++ src/test/java/com/acc/springcore/service/impl/EmployeeServiceImpl2.java
@@ -1,8 +1,10 @@
 package com.acc.springcore.service.impl;
 
 import com.acc.springcore.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
+@Qualifier("emp2")
 public class EmployeeServiceImpl2 implements EmployeeService {
 }
